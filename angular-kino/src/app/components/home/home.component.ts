02575d272707109ebfb36f0d1bc334bf7fb13e1b
junqy <popek1814@gmail.com>
2022-01-22 20:18:59 +0100

    drobne poprawki wizualne

--- angular-kino/src/app/components/home/home.component.ts
+++ angular-kino/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Screen } from 'src/app/Screen';
+import { ScreenService } from 'src/app/services/screen.service';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  screens: Screen[] = []
   constructor() { }
 
   ngOnInit(): void {
+    
   }
+
+  
 
 }
